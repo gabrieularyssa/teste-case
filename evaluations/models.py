a715c8422182b0c269d3e64dec580b00f07cec8e
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-05-02 15:49:26 -0300

    fix: evaluations model

--- evaluations/models.py
+++ evaluations/models.py
@@ -2,12 +2,27 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class EvaluationQuestion(models.Model):
+    evaluations = models.ForeignKey(
+        "Evaluation",
+        on_delete=models.CASCADE,
+    )
+    questions = models.ForeignKey(
+        "questions.Question",
+        on_delete=models.CASCADE,
+    )
+    score = models.IntegerField()
+
+
 class Evaluation(models.Model):
     title = models.CharField(max_length=50)
 
     questions = models.ManyToManyField(
         "questions.Question",
         related_name="evaluations",
+        through="EvaluationQuestion",
     )
 
     def __repr__(self) -> str:
