760a4218dc069cc0ea3f025024e7f179b21209c3
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-05-09 15:38:57 -0300

    update model score

--- scores/models.py
+++ scores/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Score(models.Model):
     user_id = models.IntegerField(default=1001)
     evaluation = models.ForeignKey("evaluations.Evaluation", on_delete=models.CASCADE)
-    total_score = models.IntegerField()
-    finish = models.BooleanField()
+    total_score = models.IntegerField(default=0)
+    finish = models.BooleanField(default=False)
