c444c235a6cbafe09a6dfbdb75b6ab72406932b2
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-05-11 10:42:27 -0300

    update model answer

--- answers/models.py
+++ answers/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Answer(models.Model):
     user_id = models.IntegerField(default=1001)
+    evaluation = models.ForeignKey("evaluations.Evaluation", on_delete=models.CASCADE)
     question = models.OneToOneField("questions.Question", on_delete=models.CASCADE)
     # score = models.ForeignKey("scores.Score", on_delete=models.CASCADE)
     question_option = models.OneToOneField(
