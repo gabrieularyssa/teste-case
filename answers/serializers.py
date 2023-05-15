9f9d856e3841082aa777f498f4171acc3d7e6fe8
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-05-15 09:40:28 -0300

    router answer - payload format

--- answers/serializers.py
+++ answers/serializers.py
@@ -7,31 +7,37 @@ from answers.models import Answer
 from evaluations.models import Evaluation
 from questions.models import Question
 
-from questions.serializers import QuestionSerializer
+# from questions.serializers import QuestionSerializer
 from questions_options.serializers import QuestionOptionSerializer
 from scores.serializers import ScoreSerializer
 from evaluations.serializers import EvaluationSerializer
 
 
+class QuestionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    question_option_id = serializers.IntegerField()
+
+
 class AnswerSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    # id = serializers.IntegerField(read_only=True)
     user_id = serializers.IntegerField(required=False, default=1001)
-    question = QuestionSerializer(required=False)
-    question_option = QuestionOptionSerializer(required=False)
+    questions = QuestionSerializer(required=False, many=True)
+    # question_option = QuestionOptionSerializer(required=False)
     # score = ScoreSerializer(required=False, default=0)
     evaluation = EvaluationSerializer(required=False)
 
-    question_id = serializers.IntegerField()
-    question_option_id = serializers.IntegerField()
+    # question_id = serializers.IntegerField()
+    # question_option_id = serializers.IntegerField()
     evaluation_id = serializers.IntegerField()
     # score_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Answer
         fields = [
-            "id",
-            "question_id",
-            "question_option_id",
+            # "id",
+            # "question_id",
+            # "question_option_id",
+            "questions",
             "evaluation_id",
             # "score_id",
         ]
@@ -40,21 +46,34 @@ class AnswerSerializer(serializers.Serializer):
     def create(self, validated_data):
         body = {**validated_data}
         evaluation = body.get("evaluation_id")
-        question = body.get("question_id")
-        question_option = body.get("question_option_id")
+        questions = body.get("questions")
+        # print(questions)
+        # question_option = body.get("question_option_id")
 
         # score_id = body.get("score_id")
 
-        try:
-            evaluation = get_object_or_404(Evaluation, pk=evaluation)
-        except Http404:
-            raise NotFound("Evaluation not found")
+        # try:
+        #     evaluation = get_object_or_404(Evaluation, pk=evaluation)
+        # except Http404:
+        #     raise NotFound("Evaluation not found")
 
-        try:
-            question = get_object_or_404(Question, pk=question)
-        except Http404:
-            raise NotFound("Question not found")
+        # try:
+        #     question = get_object_or_404(Question, pk=question)
+        # except Http404:
+        #     raise NotFound("Question not found")
 
-        answer = Answer.objects.create(**body)
+        # try:
+        #     question_option = get_object_or_404(Question, pk=question_option)
+        # except Http404:
+        #     raise NotFound("Question Option not found")
 
+        question_list = []
+        for question in questions:
+            answer = Answer.objects.create(
+                question_id=question.get("id"),
+                question_option_id=question.get("question_option_id"),
+                evaluation_id=evaluation,
+            )
+            question_list.append(answer)
+            # print(question)
         return answer
