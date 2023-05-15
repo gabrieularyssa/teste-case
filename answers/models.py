6988dd08ed00156a6e13b72cf8771c38250a9c57
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-05-15 10:44:38 -0300

    update relationship answers

--- answers/models.py
+++ answers/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Answer(models.Model):
     user_id = models.IntegerField(default=1001)
     evaluation = models.ForeignKey("evaluations.Evaluation", on_delete=models.CASCADE)
-    question = models.OneToOneField("questions.Question", on_delete=models.CASCADE)
+    question = models.ForeignKey("questions.Question", on_delete=models.CASCADE)
     # score = models.ForeignKey("scores.Score", on_delete=models.CASCADE)
-    question_option = models.OneToOneField(
+    question_option = models.ForeignKey(
         "questions_options.QuestionOption", on_delete=models.CASCADE
     )
