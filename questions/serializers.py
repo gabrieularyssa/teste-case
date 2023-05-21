4b17cf237d4780f3705b134792d00ae0b3345da5
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2023-05-21 13:42:39 -0300

    route(GET-all/POST) question

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from questions.models import Question
+from .models import Question
 
 
 class QuestionSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     content = serializers.CharField(max_length=300)
+
+    class Meta:
+        model = Question
+        filds = ["content"]
+        read_only_field = ["id"]
+
+    def create(self, validated_data):
+        body = {**validated_data}
+        content = body.get("content")
+        question = Question.objects.create(content=content)
+
+        return question
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .serializers import QuestionSerializer
+from .models import Question
 
-# Create your views here.
+import json
+
+
+class QuestionView(APIView):
+    def get(self, request: Request) -> Response:
+        question_obj = Question.objects.all()
+        serializer = QuestionSerializer(question_obj, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        body = json.loads(request.body)
+        serializer = QuestionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
